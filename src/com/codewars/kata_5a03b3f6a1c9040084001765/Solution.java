1b87fdf147decc439268e9bf1515bd395a012ef2
RomanPryshchepa <roman.pryshchepa@gmail.com>
2024-09-10 20:08:45 +0200

    kata_5a03b3f6a1c9040084001765

---
+++ src/com/codewars/kata_5a03b3f6a1c9040084001765/Solution.java
@@ -0,0 +1,11 @@
+package com.codewars.kata_5a03b3f6a1c9040084001765;
+
+/*
+Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+ */
+public class Solution {
+
+  public static int angle(int n) {
+    return (n - 2) * 180;
+  }
+}
