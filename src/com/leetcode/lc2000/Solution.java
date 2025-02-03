99840f46dd45f7e7249be6b6f9cc7a7b366d94c3
RomanPryshchepa <roman.pryshchepa@gmail.com>
2025-02-03 16:32:27 +0100

    2000. Reverse Prefix of Word

---
+++ src/com/leetcode/lc2000/Solution.java
@@ -0,0 +1,61 @@
+package com.leetcode.lc2000;
+
+/*
+2000. Reverse Prefix of Word
+
+Given a 0-indexed string word and a character ch, reverse the segment of word that starts at index 0 and ends at the index of the first occurrence of ch (inclusive). If the character ch does not exist in word, do nothing.
+
+    For example, if word = "abcdefd" and ch = "d", then you should reverse the segment that starts at 0 and ends at 3 (inclusive). The resulting string will be "dcbaefd".
+
+Return the resulting string.
+
+
+
+Example 1:
+
+Input: word = "abcdefd", ch = "d"
+Output: "dcbaefd"
+Explanation: The first occurrence of "d" is at index 3.
+Reverse the part of word from 0 to 3 (inclusive), the resulting string is "dcbaefd".
+
+Example 2:
+
+Input: word = "xyxzxe", ch = "z"
+Output: "zxyxxe"
+Explanation: The first and only occurrence of "z" is at index 3.
+Reverse the part of word from 0 to 3 (inclusive), the resulting string is "zxyxxe".
+
+Example 3:
+
+Input: word = "abcd", ch = "z"
+Output: "abcd"
+Explanation: "z" does not exist in word.
+You should not do any reverse operation, the resulting string is "abcd".
+
+
+
+Constraints:
+
+    1 <= word.length <= 250
+    word consists of lowercase English letters.
+    ch is a lowercase English letter.
+ */
+public class Solution {
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    System.out.println(solution.reversePrefix("abcdefd", 'd'));
+    System.out.println(solution.reversePrefix("xyxzxe", 'z'));
+    System.out.println(solution.reversePrefix("abcd", 'z'));
+  }
+
+  public String reversePrefix(String word, char ch) {
+    var chIdx = word.indexOf(ch);
+    if (chIdx == -1) {
+      return word;
+    }
+    var res = new StringBuilder(word.substring(0, chIdx + 1)).reverse();
+    res.append(word.substring(chIdx + 1));
+    return res.toString();
+  }
+}
