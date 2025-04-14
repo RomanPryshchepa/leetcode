306bed504fbb53d5060050086e7fa7a602af946c
RomanPryshchepa <roman.pryshchepa@gmail.com>
2025-04-14 16:23:36 +0200

    617. Merge Two Binary Trees

--- src/com/leetcode/tools/TreeNode.java
+++ src/com/leetcode/tools/TreeNode.java
@@ -1,15 +1,15 @@
-package com.leetcode.lc700;
+package com.leetcode.tools;
 
 public class TreeNode {
 
-  int val;
-  TreeNode left;
-  TreeNode right;
+  public int val;
+  public TreeNode left;
+  public TreeNode right;
 
   TreeNode() {
   }
 
-  TreeNode(int val) {
+  public TreeNode(int val) {
     this.val = val;
   }
 
