e31c05c6578400a61c213bea60fcbdceb01e66d0
RomanPryshchepa <roman.pryshchepa@gmail.com>
2025-09-30 17:39:07 +0200

    804. Unique Morse Code Words

---
+++ src/com/leetcode/lc804/Solution.java
@@ -0,0 +1,73 @@
+package com.leetcode.lc804;
+
+import java.util.HashSet;
+
+/*
+804. Unique Morse Code Words
+
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+
+    'a' maps to ".-",
+    'b' maps to "-...",
+    'c' maps to "-.-.", and so on.
+
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+
+Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+    For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+
+Return the number of different transformations among all words we have.
+
+
+
+Example 1:
+
+Input: words = ["gin","zen","gig","msg"]
+Output: 2
+Explanation: The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+There are 2 different transformations: "--...-." and "--...--.".
+
+Example 2:
+
+Input: words = ["a"]
+Output: 1
+
+
+
+Constraints:
+
+    1 <= words.length <= 100
+    1 <= words[i].length <= 12
+    words[i] consists of lowercase English letters.
+ */
+public class Solution {
+  private final String[] morse = new String[] { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    System.out.println(solution.uniqueMorseRepresentations(new String[] {"gin", "zen", "gig", "msg"}));
+    System.out.println(solution.uniqueMorseRepresentations(new String[] {"a"}));
+  }
+
+  public int uniqueMorseRepresentations(String[] words) {
+    var set = new HashSet<String>();
+    for (var word : words)
+      set.add(getMorse(word));
+    return set.size();
+  }
+
+  private String getMorse(String word) {
+    var sb = new StringBuilder();
+    for (var c : word.toCharArray()) {
+      sb.append(morse[c - 97]);
+    }
+    return sb.toString();
+  }
+}
