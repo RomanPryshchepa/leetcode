4347998d782abb20fe56c249745425b4ca2042ea
RomanPryshchepa <roman.pryshchepa@gmail.com>
2026-03-18 19:46:35 +0100

    88. Merge Sorted Array v2

--- src/com/leetcode/lc88/Solution.java
+++ src/com/leetcode/lc88/Solution.java
@@ -46,6 +46,10 @@ public class Solution {
     solution.merge(num, 1, new int[]{1}, 1);
     System.out.println(Arrays.toString(num));
 
+    num = new int[]{2, 0};
+    solution.merge2(num, 1, new int[]{1}, 1);
+    System.out.println(Arrays.toString(num));
+
     num = new int[]{-10, -10, -9, -9, -9, -8, -8, -7, -7, -7, -6, -6, -6, -6, -6, -6, -6, -5, -5,
         -5, -4, -4, -4, -3, -3, -2, -2, -1, -1, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 5, 5, 6, 6, 6, 6,
         7, 7, 7, 7, 8, 9, 9, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -59,25 +63,69 @@ public class Solution {
     solution.merge(num, 55, num2, 99);
     System.out.println(Arrays.toString(num));
 
+    num = new int[]{-10, -10, -9, -9, -9, -8, -8, -7, -7, -7, -6, -6, -6, -6, -6, -6, -6, -5, -5,
+            -5, -4, -4, -4, -3, -3, -2, -2, -1, -1, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 5, 5, 6, 6, 6, 6,
+            7, 7, 7, 7, 8, 9, 9, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    solution.merge2(num, 55, num2, 99);
+    System.out.println(Arrays.toString(num));
+
     num = new int[]{1, 2, 4, 5, 6, 0};
     solution.merge(num, 5, new int[]{3}, 1);
+    System.out.println(Arrays.toString(num));
+
+    num = new int[]{1, 2, 4, 5, 6, 0};
+    solution.merge2(num, 5, new int[]{3}, 1);
     System.out.println(Arrays.toString(num));
 
     num = new int[]{1, 3, 5, 0, 0, 0};
     solution.merge(num, 3, new int[]{2, 4, 6}, 3);
     System.out.println(Arrays.toString(num));
 
+    num = new int[]{1, 3, 5, 0, 0, 0};
+    solution.merge2(num, 3, new int[]{2, 4, 6}, 3);
+    System.out.println(Arrays.toString(num));
+
     num = new int[]{1, 2, 3, 0, 0, 0};
     solution.merge(num, 3, new int[]{2, 5, 6}, 3);
+    System.out.println(Arrays.toString(num));
+
+    num = new int[]{1, 2, 3, 0, 0, 0};
+    solution.merge2(num, 3, new int[]{2, 5, 6}, 3);
     System.out.println(Arrays.toString(num));
 
     num = new int[]{1};
     solution.merge(num, 1, new int[]{}, 0);
     System.out.println(Arrays.toString(num));
 
+    num = new int[]{1};
+    solution.merge2(num, 1, new int[]{}, 0);
+    System.out.println(Arrays.toString(num));
+
     num = new int[]{0};
     solution.merge(num, 0, new int[]{1}, 1);
     System.out.println(Arrays.toString(num));
+
+    num = new int[]{0};
+    solution.merge2(num, 0, new int[]{1}, 1);
+    System.out.println(Arrays.toString(num));
+  }
+
+  public void merge2(int[] nums1, int m, int[] nums2, int n) {
+    var p1 = m - 1;
+    var p2 = n - 1;
+    while (p1 >= 0 || p2 >= 0) {
+      if (p1 >= 0 && p2 >= 0 && nums1[p1] <= nums2[p2])
+        nums1[p1 + p2 + 1] = nums2[p2--];
+      else if (p1 >= 0 && p2 >= 0)
+        nums1[p1 + p2 + 1] = nums1[p1--];
+      else if (p1 >= 0)
+        nums1[p1 + p2 + 1] = nums1[p1--];
+      else
+        nums1[p1 + p2 + 1] = nums2[p2--];
+    }
   }
 
   public void merge(int[] nums1, int m, int[] nums2, int n) {
