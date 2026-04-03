7f33208343324bb594010e400e2d1f2547c273a3
RomanPryshchepa <roman.pryshchepa@gmail.com>
2026-04-03 12:46:52 +0200

    49. Group Anagrams v2

--- src/com/leetcode/lc49/Solution.java
+++ src/com/leetcode/lc49/Solution.java
@@ -38,8 +38,44 @@ public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.groupAnagrams(new String[] { "eat", "tea", "tan", "ate", "nat", "bat" }));
+        System.out.println(solution.groupAnagrams2(new String[] { "eat", "tea", "tan", "ate", "nat", "bat" }));
         System.out.println(solution.groupAnagrams(new String[] { "" }));
+        System.out.println(solution.groupAnagrams2(new String[] { "" }));
         System.out.println(solution.groupAnagrams(new String[] { "a" }));
+        System.out.println(solution.groupAnagrams2(new String[] { "a" }));
+    }
+
+    public List<List<String>> groupAnagrams2(String[] strs) {
+        var map = new HashMap<String, List<String>>();
+        var inMap = false;
+        for (String str : strs) {
+            inMap = false;
+            for (var key : map.keySet())
+                if (str.length() == key.length() && isAnagram(str, key)) {
+                    map.get(key).add(str);
+                    inMap = true;
+                }
+            if (!inMap)
+                map.put(str, new ArrayList<>() {{
+                    add(str);
+                }});
+        }
+        var res = new ArrayList<List<String>>();
+        for (var key : map.keySet())
+            res.add(map.get(key));
+        return res;
+    }
+
+    private boolean isAnagram(String str1, String str2) {
+        var letters = new int[26];
+        for (char c : str1.toCharArray())
+            letters[c - 'a']++;
+        for (char c : str2.toCharArray())
+            letters[c - 'a']--;
+        for (var i : letters)
+            if (i != 0)
+                return false;
+        return true;
     }
 
     public List<List<String>> groupAnagrams(String[] strs) {
@@ -48,7 +84,7 @@ public class Solution {
             char[] charArray = str.toCharArray();
             Arrays.sort(charArray);
             String key = String.valueOf(charArray);
-            List<String> value = res.getOrDefault(key, new ArrayList<String>());
+            List<String> value = res.getOrDefault(key, new ArrayList<>());
             value.add(str);
             res.put(key, value);
         }
