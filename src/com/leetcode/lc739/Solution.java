e804e7309eb7e3a11e14654cd1939106dcdf76dd
RomanPryshchepa <roman.pryshchepa@gmail.com>
2026-04-08 20:00:05 +0200

    739. Daily Temperatures v2 + refactoring

--- src/com/leetcode/lc739/Solution.java
+++ src/com/leetcode/lc739/Solution.java
@@ -3,6 +3,7 @@ package com.leetcode.lc739;
 import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.Stack;
 
 /*
 739. Daily Temperatures
@@ -37,24 +38,30 @@ Constraints:
 public class Solution {
 
   public static void main(String[] args) {
-    System.out.println(
-        Arrays.toString(new String[]{"0 ", "1 ", "2 ", "3 ", "4 ", "5 ", "6 ", "7 "}));
-    int[] temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
-    System.out.println(Arrays.toString(temperatures));
-    System.out.println(Arrays.toString(dailyTemperatures(temperatures)));
-    System.out.println(Arrays.toString(dailyTemperatures1(temperatures)));
+    var solution = new Solution();
+    int[] temperatures;
 
+    temperatures = new int[] {73, 74, 75, 71, 69, 72, 76, 73};
+    System.out.println(Arrays.toString(solution.dailyTemperatures(temperatures)));
+    System.out.println(Arrays.toString(solution.dailyTemperatures2(temperatures)));
     System.out.println();
-    int[] temperatures1 = {99, 99, 99, 99, 99, 99, 99, 100};
-    System.out.println(Arrays.toString(temperatures1));
-    System.out.println(Arrays.toString(dailyTemperatures(temperatures1)));
-    System.out.println(Arrays.toString(dailyTemperatures1(temperatures1)));
 
-    System.out.println(Arrays.toString(dailyTemperatures(new int[]{30, 40, 50, 60})));
-    System.out.println(Arrays.toString(dailyTemperatures(new int[]{30, 60, 90})));
+    temperatures = new int[] {30, 40, 50, 60};
+    System.out.println(Arrays.toString(solution.dailyTemperatures(temperatures)));
+    System.out.println(Arrays.toString(solution.dailyTemperatures2(temperatures)));
+    System.out.println();
+
+    temperatures = new int[] {30, 60, 90};
+    System.out.println(Arrays.toString(solution.dailyTemperatures(temperatures)));
+    System.out.println(Arrays.toString(solution.dailyTemperatures2(temperatures)));
+    System.out.println();
+
+    temperatures = new int[] {99, 99, 99, 99, 99, 99, 99, 100};
+    System.out.println(Arrays.toString(solution.dailyTemperatures(temperatures)));
+    System.out.println(Arrays.toString(solution.dailyTemperatures2(temperatures)));
   }
 
-  public static int[] dailyTemperatures(int[] temperatures) {
+  public int[] dailyTemperatures(int[] temperatures) {
     int[] res = new int[temperatures.length];
     Deque<Integer> stack = new LinkedList<>();
     stack.push(temperatures[0]);
@@ -76,19 +83,22 @@ public class Solution {
     return res;
   }
 
-
-  public static int[] dailyTemperatures1(int[] temperatures) {
-    int[] res = new int[temperatures.length];
-    for (int i = 0; i < temperatures.length - 1; i++) {
-      int j = i + 1;
-      while (j < temperatures.length) {
-        if (temperatures[j] > temperatures[i]) {
-          res[i] = j - i;
-          break;
-        }
-        j++;
+  public int[] dailyTemperatures2(int[] temperatures) {
+    var answer = new int[temperatures.length];
+    var stackTemp = new Stack<Integer>();
+    var stackIdx = new Stack<Integer>();
+    for (var i = temperatures.length - 1; i >= 0; i--) {
+      while (!stackTemp.empty() && temperatures[i] >= stackTemp.peek()) {
+        stackTemp.pop();
+        stackIdx.pop();
       }
+      if (stackIdx.empty())
+        answer[i] = 0;
+      else
+        answer[i] = stackIdx.peek() - i;
+      stackTemp.push(temperatures[i]);
+      stackIdx.push(i);
     }
-    return res;
+    return answer;
   }
 }
