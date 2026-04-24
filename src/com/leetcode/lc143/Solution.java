7b02d4c0b1e81bb2f6205acefface0cccd26aa84
RomanPryshchepa <roman.pryshchepa@gmail.com>
2026-04-24 18:10:41 +0200

    143. Reorder List v2

--- src/com/leetcode/lc143/Solution.java
+++ src/com/leetcode/lc143/Solution.java
@@ -1,6 +1,9 @@
 package com.leetcode.lc143;
 
 import com.leetcode.tools.ListNode;
+
+import java.util.Stack;
+
 import static com.leetcode.tools.ListNodeHelper.*;
 
 /*
@@ -39,9 +42,16 @@ public class Solution {
 
   public static void main(String[] args) {
     Solution solution = new Solution();
+
     ListNode head = createList(1, 2, 3, 4, 5);
     printList(head, " > ");
     solution.reorderList(head);
+    printList(head, " > ");
+    System.out.println();
+
+    head = createList(1, 2, 3, 4, 5);
+    printList(head, " > ");
+    solution.reorderList2(head);
     printList(head, " > ");
   }
 
@@ -72,6 +82,31 @@ public class Solution {
       secondList.next = tmp1;
       firstList = tmp1;
       secondList = tmp2;
+    }
+  }
+
+  public void reorderList2(ListNode head) {
+    if (head.next == null || head.next.next == null)
+      return;
+    var slow = head;
+    var fast = head.next;
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    var curr = slow.next;
+    slow.next = null;
+    var stack = new Stack<ListNode>();
+    while (curr != null) {
+      stack.push(curr);
+      curr = curr.next;
+    }
+    curr = head;
+    while (!stack.empty()) {
+      var next = curr.next;
+      curr.next = stack.pop();
+      curr.next.next = next;
+      curr = next;
     }
   }
 }
