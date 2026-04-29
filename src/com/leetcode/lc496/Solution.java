9001bcb4ec808261baaeaf8b72e8f3d9814fc173
RomanPryshchepa <roman.pryshchepa@gmail.com>
2026-04-29 09:14:11 +0200

    496. Next Greater Element I v2

--- src/com/leetcode/lc496/Solution.java
+++ src/com/leetcode/lc496/Solution.java
@@ -1,8 +1,6 @@
 package com.leetcode.lc496;
 
-import java.util.Arrays;
-import java.util.TreeMap;
-import java.util.TreeSet;
+import java.util.*;
 
 /*
 496. Next Greater Element I
@@ -49,15 +47,29 @@ Follow up: Could you find an O(nums1.length + nums2.length) solution?
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
+
         System.out.println(Arrays.toString(solution.nextGreaterElement(new int[]{4,1,2}, new int[]{1,3,4,2})));
+        System.out.println(Arrays.toString(solution.nextGreaterElement2(new int[]{4,1,2}, new int[]{1,3,4,2})));
+        System.out.println();
+
+        System.out.println(Arrays.toString(solution.nextGreaterElement(new int[]{2,4}, new int[]{1,2,3,4})));
+        System.out.println(Arrays.toString(solution.nextGreaterElement2(new int[]{2,4}, new int[]{1,2,3,4})));
+        System.out.println();
+
         System.out.println(Arrays.toString(solution.nextGreaterElement(new int[]{2,4}, new int[]{1,2,3,4})));
         System.out.println(Arrays.toString(solution.nextGreaterElement(new int[]{2,4}, new int[]{1,2,3,4})));
+        System.out.println();
+
         var num1 = new int[]{137,59,92,122,236};
         var num2 = new int[]{137,59,92,122,236};
         System.out.println(Arrays.toString(solution.nextGreaterElement(num1, num2))); // [236,92,122,236,-1]
+        System.out.println(Arrays.toString(solution.nextGreaterElement2(num1, num2))); // [236,92,122,236,-1]
+        System.out.println();
+
         num1 = new int[]{137,59,92,122,52,131,79,236,94,171,141,86,169,199,248,120,196,168,77,71,5,198,215,230,176,87,189,206,115,76,13,216,197,26,183,54,250,27,109,140,147,25,96,105,30,207,241,8,217,40,0,35,221,191,83,132,9,144,12,91,175,65,170,149,174,82,102,167,62,70,44,143,10,153,160,142,188,81,146,212,15,162,103,163,123,48,245,116,192,14,211,126,63,180,88,155,224,148,134,158,119,165,130,112,166,93,125,1,11,208,150,100,106,194,124,2,184,75,113,104,18,210,202,111,84,223,173,238,41,33,154,47,244,232,249,60,164,227,253,56,157,99,179,6,203,110,127,152,252,55,185,73,67,219,22,156,118,234,37,193,90,187,181,23,220,72,255,58,204,7,107,239,42,139,159,95,45,242,145,172,209,121,24,21,218,246,49,46,243,178,64,161,117,20,214,17,114,69,182,85,229,32,129,29,226,136,39,36,233,43,240,254,57,251,78,51,195,98,205,108,61,66,16,213,19,68,237,190,3,200,133,80,177,97,74,138,38,235,135,186,89,201,4,101,151,31,228,231,34,225,28,222,128,53,50,247};
         num2 = new int[]{137,59,92,122,52,131,79,236,94,171,141,86,169,199,248,120,196,168,77,71,5,198,215,230,176,87,189,206,115,76,13,216,197,26,183,54,250,27,109,140,147,25,96,105,30,207,241,8,217,40,0,35,221,191,83,132,9,144,12,91,175,65,170,149,174,82,102,167,62,70,44,143,10,153,160,142,188,81,146,212,15,162,103,163,123,48,245,116,192,14,211,126,63,180,88,155,224,148,134,158,119,165,130,112,166,93,125,1,11,208,150,100,106,194,124,2,184,75,113,104,18,210,202,111,84,223,173,238,41,33,154,47,244,232,249,60,164,227,253,56,157,99,179,6,203,110,127,152,252,55,185,73,67,219,22,156,118,234,37,193,90,187,181,23,220,72,255,58,204,7,107,239,42,139,159,95,45,242,145,172,209,121,24,21,218,246,49,46,243,178,64,161,117,20,214,17,114,69,182,85,229,32,129,29,226,136,39,36,233,43,240,254,57,251,78,51,195,98,205,108,61,66,16,213,19,68,237,190,3,200,133,80,177,97,74,138,38,235,135,186,89,201,4,101,151,31,228,231,34,225,28,222,128,53,50,247};
         System.out.println(Arrays.toString(solution.nextGreaterElement(num1, num2)));
+        System.out.println(Arrays.toString(solution.nextGreaterElement2(num1, num2)));
     }
 
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
@@ -91,5 +103,29 @@ public class Solution {
             result[i] = map.getOrDefault(nums1[i], -1);
         }
         return result;
+    }
+
+    public int[] nextGreaterElement2(int[] nums1, int[] nums2) {
+        var map = new HashMap<Integer, Integer>();
+        var stack = new Stack<Integer>();
+        map.put(nums2[nums2.length - 1], -1);
+        stack.push(nums2[nums2.length - 1]);
+        for (var i = nums2.length - 2; i >= 0; i--) {
+            if (nums2[i] < stack.peek())
+                map.put(nums2[i], stack.peek());
+            else {
+                while (!stack.empty() && nums2[i] > stack.peek())
+                    stack.pop();
+                if (stack.empty())
+                    map.put(nums2[i], -1);
+                else
+                    map.put(nums2[i], stack.peek());
+            }
+            stack.push(nums2[i]);
+        }
+        var res = new int[nums1.length];
+        for (var i = 0; i < nums1.length; i++)
+            res[i] = map.get(nums1[i]);
+        return res;
     }
 }
