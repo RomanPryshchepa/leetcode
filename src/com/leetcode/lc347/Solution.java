a64c94e6b0285d67250dcb5061751698cd6079c7
RomanPryshchepa <roman.pryshchepa@gmail.com>
2026-06-05 15:59:43 +0200

    347. Top K Frequent Elements

---
+++ src/com/leetcode/lc347/Solution.java
@@ -0,0 +1,73 @@
+package com.leetcode.lc347;
+
+import java.util.*;
+
+/*
+347. Top K Frequent Elements
+
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+
+Output: [1,2]
+
+Example 2:
+
+Input: nums = [1], k = 1
+
+Output: [1]
+
+Example 3:
+
+Input: nums = [1,2,1,2,1,2,3,1,3,2], k = 2
+
+Output: [1,2]
+
+
+
+Constraints:
+
+    1 <= nums.length <= 105
+    -104 <= nums[i] <= 104
+    k is in the range [1, the number of unique elements in the array].
+    It is guaranteed that the answer is unique.
+
+
+
+Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        var solution = new Solution();
+        System.out.println(Arrays.toString(solution.topKFrequent(new int[]{1,1,1,2,2,3}, 2)));
+        System.out.println(Arrays.toString(solution.topKFrequent(new int[]{1}, 1)));
+        System.out.println(Arrays.toString(solution.topKFrequent(new int[]{1,2,1,2,1,2,3,1,3,2}, 2)));
+    }
+
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            int count = map.getOrDefault(num, 0) + 1;
+            map.put(num, count);
+        }
+
+        List<Integer> values = new ArrayList<>(map.values());
+        values.sort(Comparator.reverseOrder());
+
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                if (entry.getValue() != null && entry.getValue().equals(values.get(i))) {
+                    res[i] = entry.getKey();
+                    map.put(entry.getKey(), null);
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
