2e1d0281725644915853e2fa748478061abdaa33
RomanPryshchepa <roman.pryshchepa@gmail.com>
2026-09-23 19:37:39 +0200

    33. Search in Rotated Sorted Array v2

--- src/com/leetcode/lc33/Solution.java
+++ src/com/leetcode/lc33/Solution.java
@@ -42,17 +42,29 @@ public class Solution {
     public static void main(String[] args) {
         var solution = new Solution();
         System.out.println(solution.search(new int[] {4,5,6,7,0,1,2}, 0)); // 4
+        System.out.println(solution.search2(new int[] {4,5,6,7,0,1,2}, 0)); // 4
         System.out.println(solution.search(new int[] {4,5,6,7,0,1,2}, 3)); // -1
+        System.out.println(solution.search2(new int[] {4,5,6,7,0,1,2}, 3)); // -1
         System.out.println(solution.search(new int[] {1}, 0)); // -1
+        System.out.println(solution.search2(new int[] {1}, 0)); // -1
         System.out.println(solution.search(new int[] {5,6,7,8,9,10,11,12,13,14,15,0,1,2,3,4}, 9)); // 4
+        System.out.println(solution.search2(new int[] {5,6,7,8,9,10,11,12,13,14,15,0,1,2,3,4}, 9)); // 4
         System.out.println(solution.search(new int[] {5,6,7,8,9,10,11,12,13,14,15,0,1,2,3,4}, 0)); // 11
+        System.out.println(solution.search2(new int[] {5,6,7,8,9,10,11,12,13,14,15,0,1,2,3,4}, 0)); // 11
         System.out.println(solution.search(new int[] {5,6,7,8,9,10,11,12,13,14,15,0,1,2,3,4}, 1)); // 12
+        System.out.println(solution.search2(new int[] {5,6,7,8,9,10,11,12,13,14,15,0,1,2,3,4}, 1)); // 12
         System.out.println(solution.search(new int[] {5,6,7,8,9,10,11,12,13,14,15,0,1,2,3,4}, 6)); // 1
+        System.out.println(solution.search2(new int[] {5,6,7,8,9,10,11,12,13,14,15,0,1,2,3,4}, 6)); // 1
         System.out.println(solution.search(new int[] {5,6,7,8,9,10,11,12,13,14,15,0,1,2,3,4}, 3)); // 14
+        System.out.println(solution.search2(new int[] {5,6,7,8,9,10,11,12,13,14,15,0,1,2,3,4}, 3)); // 14
         System.out.println(solution.search(new int[] {5,6,7,8,9,10,11,12,13,14,15,0,1,2,3,4}, 5)); // 0
+        System.out.println(solution.search2(new int[] {5,6,7,8,9,10,11,12,13,14,15,0,1,2,3,4}, 5)); // 0
         System.out.println(solution.search(new int[] {5,6,7,8,9,10,11,12,13,14,15,0,1,2,3,4}, 4)); // 15
+        System.out.println(solution.search2(new int[] {5,6,7,8,9,10,11,12,13,14,15,0,1,2,3,4}, 4)); // 15
         System.out.println(solution.search(new int[] {5,6,7,8,9,10,11,12,13,14,15,0,1,2,3,4}, 13)); // 8
+        System.out.println(solution.search2(new int[] {5,6,7,8,9,10,11,12,13,14,15,0,1,2,3,4}, 13)); // 8
         System.out.println(solution.search(new int[] {5,6,7,8,9,10,11,12,13,14,15,0,1,2,3,4}, 23)); // -1
+        System.out.println(solution.search2(new int[] {5,6,7,8,9,10,11,12,13,14,15,0,1,2,3,4}, 23)); // -1
     }
 
     //     0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15
@@ -88,6 +100,40 @@ public class Solution {
                         start = mid + 1;
                 }
             }
+        }
+        return -1;
+    }
+
+    public int search2(int[] nums, int target) {
+        var st = 0;
+        var end = nums.length - 1;
+        var mid = st + (end - st) / 2;
+        while (st <= end) {
+            if (nums[mid] == target)
+                return mid;
+            if (nums[st] < nums[end]) {
+                if (nums[mid] < target)
+                    st = mid + 1;
+                else
+                    end = mid - 1;
+            } else {
+                if (nums[mid] < nums[st] && nums[mid] < nums[end]) {
+                    if (nums[mid] > target)
+                        end = mid - 1;
+                    else if (nums[st] <= target)
+                        end = mid - 1;
+                    else
+                        st = mid + 1;
+                } else {
+                    if (nums[mid] < target)
+                        st = mid + 1;
+                    else if (nums[st] > target)
+                        st = mid + 1;
+                    else
+                        end = mid - 1;
+                }
+            }
+            mid = st + (end - st) / 2;
         }
         return -1;
     }
